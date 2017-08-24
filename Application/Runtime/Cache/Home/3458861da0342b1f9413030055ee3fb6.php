d11c2f5625dc3714f08904db7428fbcf9b188e92
Heaven <Heaven@macdeMacBook-Air.local>
2017-08-24 15:24:29 +0800

    ..

--- Application/Runtime/Cache/Home/3458861da0342b1f9413030055ee3fb6.php
+++ Application/Runtime/Cache/Home/3458861da0342b1f9413030055ee3fb6.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html><html lang="zh"><head data-base="/"> 
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html><html lang="zh"><head data-base="/"> 
   <meta charset="UTF-8"> 
   <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
   <meta name="renderer" content="webkit"> 
@@ -9,16 +9,72 @@
  <title>节能咨询服务,锅炉系统节能,废水处理装置,粉尘处理装置-诺亚xx环保</title>
 <meta name="keywords" content="节能咨询服务,锅炉系统节能,废水处理装置,粉尘处理装置">
 <meta name="description" content="广东省迦南节能环保科技有限公司致力于研发节能咨询服务,锅炉系统节能,废水处理装置,粉尘处理装置等节能环保装置，咨询热线：0760-89889777。"> 
-  <link href="__HOME__/css/reset.css" rel="stylesheet"> 
-  <link href="__HOME__/css/laypage.css" rel="stylesheet"> 
-  <link href="__HOME__/css/kind-editor-pub-reset.css" rel="stylesheet"> 
-  <script src="__HOME__/js/nsw.pc.min.js"></script> 
- <script src="__HOME__/js/template-sub-domain.js"></script>
-  <script src="__HOME__/js/form.js" type="text/javascript"></script> 
-<link href="__HOME__/css/58ef1a7a82ce717cec00a3d5.css" rel="stylesheet">
+  <link href="/Public/Home/css/reset.css" rel="stylesheet"> 
+  <link href="/Public/Home/css/laypage.css" rel="stylesheet"> 
+  <link href="/Public/Home/css/kind-editor-pub-reset.css" rel="stylesheet"> 
+  <script src="/Public/Home/js/nsw.pc.min.js"></script> 
+ <script src="/Public/Home/js/template-sub-domain.js"></script>
+  <script src="/Public/Home/js/form.js" type="text/javascript"></script> 
+<link href="/Public/Home/css/58ef1a7a82ce717cec00a3d5.css" rel="stylesheet">
  <body> 
   <!--头部--> 
-<include file="Public/header" />
+<div class="header"> 
+  <div class="h_top"> 
+   <span class="fr"> <a class="sc" href="javascript:addFavorite2();" rel="sidebar">收藏本站</a> 
+                     <a class="dt" href="/sitemap.html" target="_blank" title="网站地图">网站地图</a> 
+                     <a class="ly" href="/Tools/leaveword.html" title="在线留言">在线留言</a>
+  </span> 欢迎进入XX节能环保科技有限公司官网! 
+  </div> 
+  <div class="h_nr"> 
+   <div class="h_logo"> 
+    <span class="fl"><a href="/" title="迦南环保"><img alt="迦南环保" src="/Public/Home/picture/8b119ffbe26741fa8e0ec02d4f97da8b_3.jpg"></a></span> 
+    <div class="pj fl"> 
+     <h1><img alt="" src="/Public/Home/picture/topfline.jpg"></h1> 
+    </div> 
+    <div class="pz fl"> 
+     <h2> <b class="g1">国家高新技术企业认定</b> <b class="g2">节能环保解决系统专业提供商</b> </h2> 
+    </div> 
+   </div> 
+   <div class="h_lx fr">
+    全国服务热线：
+    <span>0760-89889777</span>
+   </div> 
+  </div> 
+ </div> 
+ <div class="clear"></div> 
+ <div class="h_nav"> 
+  <div class="content menu" style="display: flex;justify-content: center;"> 
+   <ul> 
+   <li>
+       <a href="<?php echo U('Index/index');?>" title="迦南首页">迦南首页</a>
+</li> 
+   <li>
+       <a href="<?php echo U('Product/VOC');?>" title="VOC废气处理装置">xxxx装置</a>
+</li>  
+   <li>
+       <a href="<?php echo U('Product/index');?>" title="产品中心">产品中心</a>
+</li> 
+   <li>
+       <a href="<?php echo U('Article/GongChengAnLi');?>" title="工程案例">工程案例</a>
+</li> 
+   <li>
+       <a href="<?php echo U('Product/ZengZhi');?>" title="增值服务">增值服务</a>
+</li> 
+   <li>
+       <a href="<?php echo U('Article/XinWen');?>" title="新闻·百科">新闻·百科</a>
+</li> 
+   <li>
+       <a href="<?php echo U('Help/About');?>" title="关于迦南">关于迦南</a>
+</li> 
+   <li>
+       <a href="<?php echo U('Help/Connected');?>" title="联系迦南">联系迦南</a>
+</li>
+   </ul> 
+  </div> 
+ </div> 
+  
+</div>
+
   <!--广告--> 
   <div>
 <div class="ty-banner-1">
@@ -336,4 +392,173 @@ window.location.href =base+'product/index' + '_' + space + '.html'
     </div> 
    </div> 
   </div> 
-  <include file="Public/footer" />
+      <div class="footer" style="background:url(/Public/Home/images/ftbg.jpg) no-repeat center 0; "> 
+ <div class="content fshw"> 
+  <div class="leftfooter"> 
+   <a href="/" title="xx环保"><img src="/Public/Home/picture/8b119ffbe26741fa8e0ec02d4f97da8b_17.jpg"></a>
+   <p>XXX环保科技有限公司 版权所有</p> 
+   <div class="dltb">
+    <dl>
+     <dt>
+      <img src="/Public/Home/picture/teld.jpg">
+     </dt>
+     <dd>
+      0760-12345
+     </dd>
+    </dl> 
+    <dl>
+     <dt>
+      <img src="/Public/Home/picture/qqtb.jpg">
+     </dt>
+     <dd>
+      12345@QQ.COM
+     </dd>
+    </dl> 
+    <dl>
+     <dt>
+      <img src="/Public/Home/picture/cz.jpg">
+     </dt>
+     <dd>
+     0760-12345
+     </dd>
+    </dl> 
+    <dl>
+     <dt>
+      <img src="/Public/Home/picture/qq.jpg">
+     </dt>
+     <dd>
+      12345
+     </dd>
+    </dl> 
+    <dl class="last">
+     <dt>
+      <img src="/Public/Home/picture/dh.jpg">
+     </dt>
+     <dd>
+      xx市xx区xx路
+     </dd>
+    </dl> 
+    <div class="clear"></div> 
+    <span> 技术支持：xx网（xxxx）百度统计 备案号：<a href="http://www.miitbeian.gov.cn/" target="_blank">暂无</a></span> 
+    <p><img alt="迦南环保" src="/Public/Home/picture/43f83a3cfcce4082a97361924f3e455c_28.jpg" title="xx环保"></p> 
+   </div> 
+  </div> 
+ </div> 
+ <div class="fnav"> 
+  <div class="content fla"> 
+  
+     <a href="/helps/JiaNanGongSiJian.html">关于迦南</a>
+
+     <a href="/product/VOC.html">VOCS催化燃烧装置</a>
+
+     <a href="/product/ShiShiJingDian.html">湿法静电除尘器</a>
+
+     <a href="/article/GongChengAnLi3068.html">解决方案</a>
+
+     <a href="/product/index.html">产品中心</a>
+
+     <a href="/sitemap.html">网站地图</a>
+  </div> 
+ </div> 
+</div>
+
+<script src="/Public/Home/js/form.js" type="text/javascript"></script>  
+    <div class="rightorder">
+    <div class="liywz">
+        <h3>在线留言</h3>
+        <span>马上留言，预约现场诊断</span>
+    </div>
+    <form class="form-horizontal" id="intentionalOrderFormId" name="intentionalOrderForm">
+        <p id="fieldsJson" style="display:none">{"contactsPhone":{"_id":"58eeed9dac308f46e81383d8","formId":"58eeed9dac308f46e81383d6","title":"手机号码","initContent":"请填写您的手机号码，填写后我司将第一时间与您通过电话取得联系","isRequired":true,"length":11,"formType":"text","fieldName":"contactsPhone","data":[],"failInfo":"验证失败提示信息","explainText":"","isVerify":true,"regular":"phone","isDisplay":true,"desc":"描述","initField":true,"orderBy":2,"_type":"FormField"},"contactsMail":{"_id":"58eeed9dac308f46e81383d9","formId":"58eeed9dac308f46e81383d6","title":"电子邮件","initContent":"请填写您的电子邮件，填写后我司将通过电子邮件给您回复","isRequired":true,"length":50,"formType":"text","fieldName":"contactsMail","data":[],"failInfo":"验证失败提示信息","explainText":"","isVerify":true,"regular":"email","isDisplay":true,"desc":"描述","orderBy":3,"_type":"FormField"},"contactsName":{"_id":"58eeed9dac308f46e81383d7","formId":"58eeed9dac308f46e81383d6","title":"姓名","initContent":"请填写您的姓名，以便于我方给您回复","isRequired":true,"length":10,"formType":"text","fieldName":"contactsName","data":[],"failInfo":"验证失败提示信息","explainText":"","isVerify":false,"regular":"","isDisplay":true,"desc":"描述","initField":true,"orderBy":1,"_type":"FormField"},"content":{"_id":"58eeed9dac308f46e81383df","formId":"58eeed9dac308f46e81383d6","title":"留言内容","initContent":"请真实详尽填写留言内容","isRequired":false,"length":300,"formType":"textarea","fieldName":"content","data":[],"failInfo":"验证失败提示信息","explainText":"","isVerify":false,"regular":"","isDisplay":true,"desc":"描述","initField":true,"orderBy":5,"_type":"FormField"}}</p>
+        <p id="formInfo" style="display:none">{"_id":"58eeed9dac308f46e81383d6","lastUpdTime":"2017-04-29 14:17:48","desc":"desc1.","name":"留言表单（系统）","timeInterval":30,"isAuditDisplay":true,"isValidate":false,"isDeleted":false,"type":1,"initForm":true,"projId":"20170413111641011711","createdTime":"2017-04-13 11:16:45","_type":"IntentionalOrderForm","vcenter":"http://centern.nsw99.com//center/intentionalOrder/add"}</p>
+        <ul class="findrow">
+            <li class="li01" id="contactsName"><span>姓 名：</span><input class="inp1" id="username" name="contactsName" type="text">*</li>
+            <li class="li02" id="contactsPhone"><span>电话：</span><input class="inp2" id="tel" name="contactsPhone" type="text">*</li>
+            <li class="li03" id="contactsMail"><span>邮 箱：</span><input class="inp3" id="email" name="contactsMail" type="text">*</li>
+            <li class="li04" id="content"><span>我要留言：</span><textarea class="inp4" id="shortdesc" name="content"></textarea></li>
+            <li class="li05" id=""> <input class="btn1 submit" name="" type="button"> <em class="btn2 reset"></em> </li>
+        </ul>
+    </form>
+</div> 
+   </div> 
+  </div> 
+  <div class="clear"></div> 
+  <div class="client-2">
+    <ul id="client-2">
+        <li class="my-kefu-qq">
+            <div class="my-kefu-main">
+                <div class="my-kefu-left">
+                    <a class="online-contact-btn" href="http://wpa.qq.com/msgrd?v=3&amp;uin=184075264&amp;site=qq&amp;menu=yes" qq="" target="_blank">
+                        <i></i>
+                        <p>QQ咨询</p>
+                    </a>
+                </div>
+                <div class="my-kefu-right"> 
+</div>
+            </div>
+        </li>
+        <!-- <li class="my-kefu-qq">-->
+        <!--    <div class="my-kefu-main">-->
+        <!--        <div class="my-kefu-left">-->
+        <!--            <a class="online-contact-btn" target="_blank" th:href="'http://wpa.qq.com/msgrd?v=3&uin='+${data.items[0].children[0].serverType.name=='QQ'?data.items[0].children[0].serverType.content:''} +'&site=qq&menu=yes'" th:if="${ not #lists.isEmpty(data.items) and #lists.size(data.items)>0 and not #lists.isEmpty(data.items[0].children)  and #lists.size(data.items[0].children)>0 and  not #strings.isEmpty(data.items[0].children[0].serverType)}">-->
+        <!--                <i></i>-->
+        <!--                <p>QQ咨询</p>-->
+        <!--            </a>-->
+        <!--        </div>-->
+        <!--        <div class="my-kefu-right">--> 
+<!--        </div>-->
+        <!--    </div>-->
+        <!--</li>-->
+        <li class="my-kefu-tel">
+            <div class="my-kefu-tel-main">
+                <div class="my-kefu-left">
+                    <i></i>
+                    <p>400电话</p>
+                </div>
+                <div class="my-kefu-tel-right" phone400="">0760-89889777</div>
+            </div>
+        </li>
+        <li class="my-kefu-liuyan">
+            <div class="my-kefu-main">
+                <div class="my-kefu-left">
+                    <a href="/Tools/leaveword.html" title="在线留言">
+                        <i></i>
+                        <p>在线留言</p>
+                    </a>
+                </div>
+                <div class="my-kefu-right"> 
+</div>
+            </div>
+        </li>
+        <li class="my-kefu-weixin">
+            <div class="my-kefu-main">
+                <div class="my-kefu-left">
+                    <i></i>
+                    <p>微信扫一扫</p>
+                </div>
+                <div class="my-kefu-right"> 
+</div>
+                <div class="my-kefu-weixin-pic">
+                    <img src="/Public/Home/picture/43f83a3cfcce4082a97361924f3e455c_28.jpg">
+                </div>
+            </div>
+        </li>
+        <li class="my-kefu-ftop">
+            <div class="my-kefu-main">
+                <div class="my-kefu-left">
+                    <a href="javascript:;">
+                        <i></i>
+                        <p>返回顶部</p>
+                    </a>
+                </div>
+                <div class="my-kefu-right"> 
+</div>
+            </div>
+        </li>
+    </ul>
+</div> 
+<script src="/Public/Home/js/58eeed9cac308f46e8138354.js" type="text/javascript"></script>
+<script>  var sysBasePath = null ;var projPageData = {};</script>
+<script src="/Public/Homejs/public.js"></script>
+  </body>
+</html>
